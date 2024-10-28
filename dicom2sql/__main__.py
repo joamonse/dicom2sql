ebd6fbfac757cedb1100dca920947f452a69f127
joamonse <31344370+joamonse@users.noreply.github.com>
2024-10-28 15:14:18 +0100

    added the option to process a file list in addition to the folders

--- dicom2sql/__main__.py
+++ dicom2sql/__main__.py
@@ -7,6 +7,7 @@ import pydicom
 import sqlalchemy
 from pydicom.errors import InvalidDicomError
 
+from dicom2sql.file_lister import get_files_from_list
 from sql.database import Database
 from file_explorer import get_files
 
@@ -44,10 +45,11 @@ if __name__ == '__main__':
     if args.init_db != '':
         upload_tags_description(args.init_db, db)
 
-    folders = list(map(lambda p: Path(p), args.paths))
+    inputs = list(map(lambda p: Path(p), args.paths))
 
-    for folder in folders:
-        for file in get_files(folder):
+    for path in inputs:
+        file_generator = get_files(path) if path.is_file() else get_files_from_list(path)
+        for file in file_generator:
             try:
                 dcm_data = pydicom.dcmread(file, stop_before_pixels=True)
             except InvalidDicomError:
