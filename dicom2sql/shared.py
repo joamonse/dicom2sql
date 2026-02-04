28467e40fa42fa53f2724dfee2ff3fe5b94b6e98
joamonse <31344370+joamonse@users.noreply.github.com>
2026-02-04 14:54:07 +0100

    Fixed how database uri is constructed

--- dicom2sql/shared.py
+++ dicom2sql/shared.py
@@ -21,9 +21,9 @@ def get_db_uri(db_config: configparser.SectionProxy, password:str) -> str:
         uri+=f":///{db_config['uri']}"
     else:
         uri += f"://{urllib.parse.quote_plus(db_config['username'])}:{urllib.parse.quote_plus(password)})@{db_config['uri']}"
-    if db_config['port']:
-        uri += f":{db_config['port']}"
-    uri += f":{db_config['database']}"
+        if db_config['port']:
+            uri += f":{db_config['port']}"
+        uri += f":{db_config['database']}"
     if db_config['options']:
         uri += f"?{db_config['options']}"
     return uri
@@ -34,8 +34,9 @@ def parse_config() -> dict:
     config.read(["default.ini", "config.ini"])
 
     values = {
-        'in_db_uri': get_db_uri(config['database.in'], os.environ['IN_PASSWORD']),
-        'out_db_uri': get_db_uri(config['database.out'], os.environ['OUT_PASSWORD'])
+        'in_db_uri': get_db_uri(config['database.in'], os.environ.get('IN_PASSWORD','')),
+        'out_db_uri': get_db_uri(config['database.out'], os.environ.get('OUT_PASSWORD',''))
     }
+    print(values)
 
     return values
